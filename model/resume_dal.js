6021d1942e0ed44dbadc58b5998ac9b8ce0b4446
student <a@a.com>
2016-12-06 22:01:48 -0800

    12/6

--- model/resume_dal.js
+++ model/resume_dal.js
@@ -20,3 +20,26 @@ exports.getById = function(resume_id, callback) {
         callback(err, result);
     });
 };
+
+exports.insert = function(params, callback) {
+    var query = 'INSERT INTO resume (resume_name, user_account_id) VALUES (?, ?)';
+
+    // the question marks in the sql query above will be replaced by the values of the
+    // the data in queryData
+    var queryData = [params.resume_name, params.account_id];
+
+    connection.query(query, queryData, function(err, result) {
+        callback(err, result);
+    });
+
+}
+
+exports.delete = function(resume_id, callback) {
+    var query = 'DELETE FROM resume WHERE resume_id = ?';
+    var queryData = [resume_id];
+
+    connection.query(query, queryData, function(err, result) {
+        callback(err, result);
+    });
+
+};
--- routes/resume_routes.js
+++ routes/resume_routes.js
@@ -1,9 +1,7 @@
-//copy code from school_routes.js and change where says school to resume
-//do the same
-
 var express = require('express');
 var router = express.Router();
 var resume_dal = require('../model/resume_dal');
+var account_dal = require('../model/account_dal');
 
 
 // View All resumes
@@ -35,5 +33,60 @@ router.get('/', function(req, res){
         });
     }
 });
+
+// Return the add a new resume form
+router.get('/add', function(req, res){
+    // passing all the query parameters (req.query) to the insert function instead of each individually
+    account_dal.getAll(function(err,result) {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.render('resume/resumeAdd', {'account': result});
+        }
+    });
+});
+
+// insert a resume record
+router.get('/insert', function(req, res){
+    // simple validation
+    if(req.query.resume_name == null) {
+        res.send('Resume Name must be provided.');
+    }
+    else if(req.query.account_id == null) {
+        res.send('An Account must be selected');
+    }
+    else {
+        // passing all the query parameters (req.query) to the insert function instead of each individually
+        resume_dal.insert(req.query, function(err,result) {
+            if (err) {
+                res.send(err);
+            }
+            else {
+                //poor practice, but we will handle it differently once we start using Ajax
+                res.redirect(302, '/resume/all');
+            }
+        });
+    }
+});
+
+// Delete a resume for the given resume_id
+router.get('/delete', function(req, res){
+    if(req.query.resume_id == null) {
+        res.send('resume_id is null');
+    }
+    else {
+        resume_dal.delete(req.query.resume_id, function(err, result){
+            if(err) {
+                res.send(err);
+            }
+            else {
+                //poor practice, but we will handle it differently once we start using Ajax
+                res.redirect(302, '/resume/all');
+            }
+        });
+    }
+});
+
 
 module.exports = router;
