e3a29bf7b789748a708030b0b1e49d175a0cedd5
a <@@>
2016-12-12 19:31:24 -0800

    Fixed BOOKS FML

--- app.js
+++ app.js
@@ -7,12 +7,6 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var books = require('./routes/book_routes');
-var school = require('./routes/school_routes');
-var account = require('./routes/account_routes');
-var resume = require('./routes/resume_routes');
-var company = require('./routes/company_routes');
-var address = require ('./routes/address_routes');
-var skill = require('./routes/skill_routes');
 
 var app = express();
 
@@ -31,12 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/books', books);
-app.use('/school', school);
-app.use('/account', account);
-app.use('/resume', resume);
-app.use('/company', company);
-app.use('/address', address);
-app.use('/skill', skill);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ model/db_connection.js
@@ -0,0 +1,6 @@
+exports.config = {
+    user: 'tarellano',
+    password: 'su4taylor',
+    host: 'blue.cs.sonoma.edu',
+    database: 'tarellano'
+};
